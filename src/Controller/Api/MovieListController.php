1974011c9d8f890cc89a06fa2fb86cee3cf21463
Kleber Salgado <kas_ecu@yahoo.com>
2021-05-28 09:31:44 +0200

    unused use deleted

--- src/Controller/Api/MovieListController.php
+++ src/Controller/Api/MovieListController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Api;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/api", name="_api")
